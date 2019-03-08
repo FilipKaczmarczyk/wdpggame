400b159a65009afbf365173811af29cba79f5c95
HappyArroW <stifmeister0700@gmail.com>
2019-03-08 16:42:13 +0100

    v 0.115

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,10 +6,14 @@ public class Player : MonoBehaviour
 {
 	private Rigidbody2D playerRigidbody;
 
-	private Animator myAnimator;
+	private Animator playerAnimator;
 
 	[SerializeField]
 	private float movementSpeed = 10f;
+
+	private bool attack;
+
+	private bool slide;
 
 	private bool directionRight;
 
@@ -19,31 +23,82 @@ public class Player : MonoBehaviour
     {
 		directionRight = true;
 		playerRigidbody = GetComponent<Rigidbody2D>();
-		myAnimator = GetComponent<Animator>();
+		playerAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+	void Update()
+	{
+		HandleInput();
+	}
+
+	void FixedUpdate()
     {
 		float horizontal = Input.GetAxis("Horizontal");
 		HandleMovement(horizontal);
 		Flip(horizontal);
+		HandleAttacks();
+		ResetValues();
+
     }
 
 	private void HandleMovement(float horizontal)
 	{
-		playerRigidbody.velocity = new Vector2(horizontal * movementSpeed, playerRigidbody.velocity.y);
-		myAnimator.SetFloat("Player_speed", Mathf.Abs(horizontal));
+		if (!playerAnimator.GetBool("Player_slide") && !this.playerAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Player_attack"))
+		{
+			playerRigidbody.velocity = new Vector2(horizontal * movementSpeed, playerRigidbody.velocity.y);
+		}
+
+		playerAnimator.SetFloat("Player_speed", Mathf.Abs(horizontal));
+
+		if (slide && !this.playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Player_slide"))
+		{
+			playerAnimator.SetBool("Player_slide", true);
+		}
+		else if (!this.playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Player_slide"))
+		{
+			playerAnimator.SetBool("Player_slide", false);
+		}
+	}
+
+	private void HandleAttacks()
+	{
+		if(attack && !this.playerAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Player_attack") && !this.playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Player_slide"))
+		{
+			playerAnimator.SetTrigger("Player_attack");
+			playerRigidbody.velocity = Vector2.zero;
+		}
+	}
+
+	private void HandleInput()
+	{
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			attack = true;
+		}
+
+		if (Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			slide = true;
+		}
 	}
 
 	private void Flip(float horizontal)
 	{
 		if((horizontal > 0 && !directionRight) || (horizontal < 0 && directionRight))
 		{
-			directionRight = !directionRight;
-			Vector3 theScale = transform.localScale;
-			theScale.x *= -1;
-			transform.localScale = theScale;
+			if(!this.playerAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Player_attack") && !this.playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Player_slide"))
+			{
+				directionRight = !directionRight;
+				Vector3 theScale = transform.localScale;
+				theScale.x *= -1;
+				transform.localScale = theScale;
+			}
 		}
+	}
+
+	private void ResetValues()
+	{
+		attack = false;
+		slide = false;
 	}
 }
