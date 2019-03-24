fb84d45292b7eab675b6eca7903b495f4413f377
Tomasz Nocoń <tomasz13nocon@gmail.com>
2019-03-24 13:41:58 +0100

    szejder dziala, font dodany

--- Assets/Scripts/PostProcessing.cs
+++ Assets/Scripts/PostProcessing.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class PostProcessing : MonoBehaviour
 {
     public Material mat;
-    public float brightness = 1.0f;
+    private float brightness = 1.0f;
+    private float contrast = 1.0f;
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        //Debug.Log(brightness);
-        mat.SetFloat("_Brightness", 1.2f);
+        mat.SetFloat("_Brightness", brightness);
         Graphics.Blit(source, destination, mat);
     }
 
     public void SetBrightness(float brightness)
     {
-        Debug.Log("QWE");
         this.brightness = brightness;
-        Debug.Log(this.brightness);
+    }
+
+    public void SetContrast(float contrast)
+    {
+        this.contrast = contrast;
     }
 }
